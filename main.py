8963e8685b4c5d7cede2e40529e9975794ca3bfa
Algor <k.moret.slv@gmail.com>
2022-03-25 10:29:45 +0000

    Get page content & parse it

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
-print('Hello World!')
-print('Ready for scrapping!')
+import requests
+from bs4 import BeautifulSoup
 
+# Variables
+site_url = 'http://books.toscrape.com/'
+product_page_url = 'http://books.toscrape.com/catalogue/the-wild-robot_288/index.html'
+
+# Get page content
+page = requests.get(product_page_url)
+page_content = page.content
+
+# Convert page to Object
+soup = BeautifulSoup(page_content, 'html.parser')
