3ed1fd55689c8b9d9eb92978882c8a9b1431ed83
Algor <k.moret.slv@gmail.com>
2022-03-25 18:02:01 +0000

    Refactor to return soup object from url

--- main.py
+++ main.py
@@ -6,12 +6,17 @@ import csv
 site_url = 'http://books.toscrape.com/'
 product_page_url = 'http://books.toscrape.com/catalogue/the-wild-robot_288/index.html'
 
-# Get page content
-page = requests.get(product_page_url)
-page_content = page.content
 
-# Convert page to Object
-soup = BeautifulSoup(page_content, 'html.parser')
+def url_to_soup_object(url):
+	# Get page content
+	page = requests.get(url)
+	page_content = page.content
+
+	# Convert page to Object
+	return BeautifulSoup(page_content, 'html.parser')
+
+
+soup = url_to_soup_object(product_page_url)
 
 ###
 # Extract all needed information
