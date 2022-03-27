946c8d4d454b7c357278c0f1ff3d1e62981c9dcb
slv-improver <k.moret.slv@gmail.com>
2022-03-27 17:11:59 +0000

    Rename variables

--- main.py
+++ main.py
@@ -25,16 +25,18 @@ for category in categories:
 	# Get list of product page URL from category page
 	category_page_content = get_page_content(category_url)
 	product_urls = get_product_page_url(category_page_content, category_url)
-	product_data = []
-	for page_url in product_urls:
-		page_content = get_page_content(page_url)
-		product_information = extract_data(page_content, category_title)
-		product_information.insert(0, page_url)
-		product_data.append(product_information)
+	product_data_list = []
+	for url in product_urls:
+		page_content = get_page_content(url)
+		# Get single product information
+		product_info = extract_data(page_content, category_title)
+		product_info.insert(0, url)
+		# Add product info to list of multiple product information
+		product_data_list.append(product_info)  # [['info', info], ['info', info], ['info', info], ['info', info]]
 
 	###
 	# Export data to csv file
 	###
-	data_to_csv(product_data, category_title)
+	data_to_csv(product_data_list, category_title)
 
 print(datetime.now().strftime("%H:%M:%S"))
